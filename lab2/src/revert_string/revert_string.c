7f9d171c090623f9467f8cd56699b47531a9a117
nadezhkinaa <161367006+nadezhkinaa@users.noreply.github.com>
2024-10-04 16:57:52 +0000

    lab2 finish

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int len=strlen(str);
+	for (int i=0; i<len/2;i++){
+		char symbol=str[i];
+		str[i]=str[len-1-i];
+		str[len-1-i]=symbol;
+	}
 }
 
