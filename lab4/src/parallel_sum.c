d83039da606e877ca9c2c2a90fda84db4aaf9b34
nadezhkinaa <161367006+nadezhkinaa@users.noreply.github.com>
2024-11-14 15:53:53 +0000

    lab4.3

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,20 +1,22 @@
+#include <ctype.h>
+#include <getopt.h>
+#include <limits.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 
-#include <pthread.h>
+#include "utils.h"
+#include "sum.h"
 
-struct SumArgs {
-  int *array;
-  int begin;
-  int end;
-};
-
-int Sum(const struct SumArgs *args) {
-  int sum = 0;
-  // TODO: your code here 
-  return sum;
-}
 
 void *ThreadSum(void *args) {
   struct SumArgs *sum_args = (struct SumArgs *)args;
@@ -22,29 +24,84 @@ void *ThreadSum(void *args) {
 }
 
 int main(int argc, char **argv) {
-  /*
-   *  TODO:
-   *  threads_num by command line arguments
-   *  array_size by command line arguments
-   *	seed by command line arguments
-   */
-
-  uint32_t threads_num = 0;
-  uint32_t array_size = 0;
-  uint32_t seed = 0;
+ int threads_num = -1;
+  int array_size = -1;
+  int seed = -1;
   pthread_t threads[threads_num];
+  while (true) {
+    static struct option options[] = {{"seed", required_argument, 0, 0},
+                                      {"array_size", required_argument, 0, 0},
+                                      {"threads_num", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
 
-  /*
-   * TODO:
-   * your code here
-   * Generate array here
-   */
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "f", options, &option_index);
 
+    if (c == -1) break;
+
+    switch (c) {
+      case 0:
+        switch (option_index) {
+          case 0:
+            seed = atoi(optarg);
+            if (seed <= 0) {
+              printf("seed must be a positive number!\n");
+              return 1;
+            }
+            break;
+          case 1:
+            array_size = atoi(optarg);
+            if (array_size <= 0) {
+              printf("array size must be a positive number!\n");
+              return 1;
+            }
+            break;
+          case 2:
+            threads_num = atoi(optarg);
+            if (threads_num <= 0) {
+              printf("threads_num must be a positive number!\n");
+              return 1;
+            }
+            break;
+
+          defalut:
+            printf("Index %d is out of options\n", option_index);
+        }
+        break;
+
+      case '?':
+        break;
+
+      default:
+        printf("getopt returned character code 0%o?\n", c);
+    }
+  }
+
+  if (optind < argc) {
+    printf("Has at least one no option argument\n");
+    return 1;
+  }
+
+  if (seed == -1 || array_size == -1 || threads_num == -1) {
+    printf(
+        "Usage: %s --seed \"num\" --array_size \"num\" --threads_num \"num\" "
+        "\n",
+        argv[0]);
+    return 1;
+  }
   int *array = malloc(sizeof(int) * array_size);
+  GenerateArray(array, array_size, seed);
 
+  struct timeval start_time;
+  gettimeofday(&start_time, NULL);
+
+  int part = array_size / threads_num;
   struct SumArgs args[threads_num];
   for (uint32_t i = 0; i < threads_num; i++) {
-    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args)) {
+    args[i].begin = i * part;
+    args[i].end = (i == threads_num - 1) ? array_size : (i + 1) * part;
+    args[i].array = array;
+    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args[i])) {
       printf("Error: pthread_create failed!\n");
       return 1;
     }
@@ -57,7 +114,15 @@ int main(int argc, char **argv) {
     total_sum += sum;
   }
 
+  struct timeval finish_time;
+  gettimeofday(&finish_time, NULL);
+
+  double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
+  elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
+
   free(array);
   printf("Total: %d\n", total_sum);
+  printf("Elapsed time: %fms\n", elapsed_time);
+
   return 0;
 }
